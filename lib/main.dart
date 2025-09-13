90dd563c6e86c09068e55fc258c2df1ab11270f4
fauzan-arift <fauzan.ariftricahya@gmail.com>
2025-09-13 14:30:35 +0700

    Implement multi-provider setup and theme configuration in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_app/data/api/api_services.dart';
+import 'package:restaurant_app/provider/detail/add_review_provider.dart';
+import 'package:restaurant_app/provider/detail/restaurant_detail_provider.dart';
+import 'package:restaurant_app/screen/detail/detail_screen.dart';
+import 'package:restaurant_app/screen/home/home_screen.dart';
+import 'package:restaurant_app/style/theme/restaurant_theme.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app/provider/home/restaurant_list_provider.dart';
+import 'package:restaurant_app/static/navigation_route.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(create: (context) => ApiService()),
+        ChangeNotifierProvider(
+          create: (context) =>
+              RestaurantListProvider(context.read<ApiService>()),
+        ),
+        ChangeNotifierProvider(
+          create: (context) =>
+              RestaurantDetailProvider(context.read<ApiService>()),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => AddReviewProvider(context.read<ApiService>()),
+        ),
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +36,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Restaurant App',
+      theme: RestaurantTheme.lightTheme,
+      darkTheme: RestaurantTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      initialRoute: NavigationRoute.mainRoute.name,
+      routes: {
+        NavigationRoute.mainRoute.name: (context) => const HomeScreen(),
+        NavigationRoute.detailRoute.name: (context) {
+          final args =
+              ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+          final restaurantId = args['id'] ?? '';
+          return DetailScreen(restaurantId: restaurantId);
+        },
+      },
     );
   }
 }
